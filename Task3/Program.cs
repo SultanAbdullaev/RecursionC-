ec9512fff238eb899be643b19db4fffb4c8b2988
Sultan Abdullaev <sultan230199@yandex.ru>
2024-02-18 17:51:53 +0300

    Added the Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+
+void ReversArray(int i)
+{
+    Console.Write($"{array[i]} ");
+
+    if (i > 0)
+    {
+        ReversArray(--i);
+    }
+}
+ReversArray(array.Length - 1);
