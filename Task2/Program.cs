d33bdf79f944ca0320e240c63902ecb740542cd4
Sultan Abdullaev <sultan230199@yandex.ru>
2024-02-18 17:51:43 +0300

    Added the Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int AckermannFun(int m, int n)
+{
+    int A = 0;
+    if (m == 0)
+    {
+        A = n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        A = AckermannFun(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        A = AckermannFun(m - 1, AckermannFun(m, n - 1));
+    }
+    return A;
+}
+int M = 3;
+int N = 4;
+Console.Write(AckermannFun(M, N));
